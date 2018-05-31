82560077e0cc16212d2beebcc5b9b785fc428bb1
Vloon <pleunscholten@hotmail.com>
2018-05-31 12:25:30 +0200

    Added very basic stuff

--- Assignment4/SPML4/src/spml4/Main.java
+++ Assignment4/SPML4/src/spml4/Main.java
@@ -2,34 +2,51 @@ package spml4;
 
 /**
  * This main is for testing purposes (and to show you how to use the MDP class).
- * 
+ *
  * @author Jered Vroon
  *
  */
 public class Main {
 
-	/**
-	 * @param args, not used
-	 */
-	public static void main(String[] args) {
-		MarkovDecisionProblem mdp = new MarkovDecisionProblem();
-		mdp.setInitialState(0, 0);
-		for (int i = 0; i < 15; i++){
-			mdp.performAction(Action.UP);
-			mdp.performAction(Action.UP);
-			mdp.performAction(Action.RIGHT);
-			mdp.performAction(Action.RIGHT);
-			mdp.performAction(Action.RIGHT);
-			mdp.restart();
-		}
-		
-		MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
-		mdp2.setField(5, 5, Field.REWARD);
-		for (int i = 0; i < 100; i++){
-			mdp2.performAction(Action.UP);
-			mdp2.performAction(Action.RIGHT);
-			mdp2.performAction(Action.DOWN);
-			mdp2.performAction(Action.LEFT);
-		}
-	}
+    /**
+     * @param args, not used
+     */
+    public static void main(String[] args) {
+        MarkovDecisionProblem mdp = new MarkovDecisionProblem();
+        mdp.setInitialState(0, 0);
+        for (int i = 0; i < 15; i++) {
+            mdp.performAction(Action.UP);
+            mdp.performAction(Action.UP);
+            mdp.performAction(Action.RIGHT);
+            mdp.performAction(Action.RIGHT);
+            mdp.performAction(Action.RIGHT);
+            mdp.restart();
+        }
+
+        MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
+        mdp2.setField(5, 5, Field.REWARD);
+        for (int i = 0; i < 100; i++) {
+            mdp2.performAction(Action.UP);
+            mdp2.performAction(Action.RIGHT);
+            mdp2.performAction(Action.DOWN);
+            mdp2.performAction(Action.LEFT);
+        }
+    }
+    
+    /*
+    
+    Initialize:
+    valueFunction is double array (same dim as mdp) with all 0
+    
+    Q: what is k and what is S?
+    
+    */
+    
+    private double[][] ValueIterations(MarkovDecisionProblem mdp) {
+        double[][] valueFunction = new double[mdp.getWidth()][mdp.getHeight()];
+        
+        
+        return null;
+    }
+    
 }
