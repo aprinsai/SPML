e4f38e2bc332e5c2965dd77fbf43da0dc98a8c27
Vloon <pleunscholten@hotmail.com>
2018-06-15 18:38:37 +0200

    Almost done?

--- Assignment4/SPML4/src/spml4/Main.java
+++ Assignment4/SPML4/src/spml4/Main.java
@@ -21,9 +21,43 @@ public class Main {
     private static void runQlearner() {
         MarkovDecisionProblem mdp = new MarkovDecisionProblem();
         mdp.setInitialState(0, 0);
-        
-        Qlearner qLearner = new Qlearner(mdp, 100, 0.8, 0.7);
 
+        Qlearner qLearner = new Qlearner(mdp, 10000, 0.8, 0.2, 0.3);
+        Action[][] policy = qLearner.run();
+        Quple[][] qFunction = qLearner.getQFunction();
+
+        for (int y = mdp.getHeight() - 1; y >= 0; y--) {
+            for (int x = 0; x < mdp.getWidth(); x++)
+                System.out.printf("%s: \t %.4s \t", qFunction[x][y].getAction(), qFunction[x][y].getValue());
+            System.out.println("");
+        }
+
+        for (int i = 0; i < 100; i++) {
+            mdp.performAction(policy[mdp.getStateXPosition()][mdp.getStateYPostion()]);
+            if (mdp.isTerminated())
+                mdp.restart();
+        }
+        System.out.println("");
+        
+        // Second one does not work very well
+        MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
+        mdp2.setField(5, 5, Field.REWARD);
+        Qlearner qLearner2 = new Qlearner(mdp2, 100000, 0.8, 0.2, 0.2);
+        Action[][] policy2 = qLearner2.run();
+        Quple[][] qFunction2 = qLearner2.getQFunction();
+
+        for (int y = mdp2.getHeight() - 1; y >= 0; y--) {
+            for (int x = 0; x < mdp2.getWidth(); x++)
+                System.out.printf("%s: \t %.4s \t", qFunction2[x][y].getAction(), qFunction2[x][y].getValue());
+            System.out.println("");
+        }
+
+        for (int i = 0; i < 100; i++) {
+            mdp2.performAction(policy2[mdp2.getStateXPosition()][mdp2.getStateYPostion()]);
+            if (mdp2.isTerminated())
+                mdp2.restart();
+        }
+        
     }
 
     private static void runValueIterator() {
--- Assignment4/SPML4/src/spml4/Qlearner.java
+++ Assignment4/SPML4/src/spml4/Qlearner.java
@@ -17,83 +17,77 @@ public class Qlearner {
     private final int nrOfIterations;
     private final double learningRate;
     private final double discount;
+    private final double epsilon;
+    private final Random rnd = new Random();
 
     private int currX;
     private int currY;
-    
-    public Qlearner(MarkovDecisionProblem mdp, int nrOfIterations, double discount, double alpha) {
+
+    public Qlearner(MarkovDecisionProblem mdp, int nrOfIterations, double discount, double learningRate, double epsilon) {
         this.mdp = mdp;
         intialize();
         this.nrOfIterations = nrOfIterations;
         this.discount = discount;
-        this.learningRate = alpha;
+        this.learningRate = learningRate;
+        this.epsilon = epsilon;
     }
 
     private void intialize() {
         qFunction = new Quple[mdp.getWidth()][mdp.getHeight()];
 //        nextQFunction = new Quple[mdp.getWidth()][mdp.getHeight()];
         for (int x = 0; x < mdp.getWidth(); x++)
-            for (int y = 0; y < mdp.getHeight(); y++) {
-                qFunction[x][y] = new Quple(0.0, null);
-//                nextQFunction[x][y] = new Quple(0.0, null);
-            }
-        currX = mdp.getStateXPosition();
-        currY = mdp.getStateYPostion();
-
+            for (int y = 0; y < mdp.getHeight(); y++)
+                if (mdp.getField(x, y) == Field.REWARD)
+                    qFunction[x][y] = new Quple(1.0, null);
+                else if (mdp.getField(x, y) == Field.NEGREWARD)
+                    qFunction[x][y] = new Quple(-1.0, null);
+                else
+                    qFunction[x][y] = new Quple(0.0, Action.UP);
     }
 
-    /*
-    
-    Initialize:
-    valueFunction is double array (same dim as mdp) with all 0
-    
-    Q: what is k and what is S?
-    
-     */
     public Action[][] run() {
         //Start in random initial state?
-        for (int k = 0; k < nrOfIterations; k++)
+        for (int k = 0; k < nrOfIterations; k++) {
+            currX = mdp.getStateXPosition();
+            currY = mdp.getStateYPostion();
+
             while (!isFinalState()) {
-                Action action = getRandomAction();
+//                System.out.printf("[%d,%d] : %f\n",currX,currY,qFunction[currX][currY].getValue());
+                //Perform random action and get state
+                Action action = getGreedyAction();
                 mdp.setDeterministic();
                 Point p = mdp.tryPerformAction(action, currX, currY);
                 mdp.setStochastic();
                 int newX = p.x;
                 int newY = p.y;
+
+                //Get the data from that new state
                 double reward = mdp.getReward(newX, newY);
                 double q = qFunction[currX][currY].getValue();
-                double maxQ = getMaxQ(newX, newY);
+                double maxQ = getMaxQ(newX, newY).getValue();
                 qFunction[currX][currY] = new Quple(q + learningRate * (reward + discount * maxQ - q), action);
-                // Q(s,a) = q + learningRate * (reward + discount * maxQ() - q)
+
                 //Update position
                 currX = newX;
                 currY = newY;
             }
+        }
         return getPolicy();
     }
 
-    private double getMaxQ(int newX, int newY) {
+    private Quple getMaxQ(int newX, int newY) {
         Quple[] qValues = new Quple[Action.values().length];
         for (int i = 0; i < qValues.length; i++) {
             Action a = Action.values()[i];
             mdp.setDeterministic();
-            Point p =  mdp.tryPerformAction(a, newX, newY);
+            Point p = mdp.tryPerformAction(a, newX, newY);
             mdp.setStochastic();
-            qValues[i] = new Quple(qFunction[p.x][p.y].getValue(),a);
+            qValues[i] = new Quple(qFunction[p.x][p.y].getValue(), a);
         }
-        return getMax(qValues).getValue();
-    }
-    
-    public void printArray() {
-        for (Quple[] valueFunction1 : qFunction) {
-            for (Quple value : valueFunction1)
-                System.out.print(value.getValue() + ", ");
-            System.out.println("");
-        }
-        System.out.println("");
+        return getMax(qValues);
     }
 
-    public Quple[][] getValueIterations() {
+    public Quple[][] getQFunction() {
         return qFunction;
     }
 
@@ -122,8 +116,16 @@ public class Qlearner {
     }
 
     private Action getRandomAction() {
-        Random rnd = new Random();
         int index = rnd.nextInt(Action.values().length);
         return Action.values()[index];
+    }
+    
+    private Action getGreedyAction() {
+        double prob = rnd.nextDouble();
+        if (prob > epsilon) {
+            return getMaxQ(currX, currY).getAction();
+        }
+        else
+            return getRandomAction();
     }
 }
