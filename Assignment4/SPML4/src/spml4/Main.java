2dab1c58f4ed93af7900efda08d5e2ff7be2d10c
Anouk <a.prins@student.ru.nl>
2018-06-23 10:54:32 +0200

    change learning rates to optimal

--- Assignment4/SPML4/src/spml4/Main.java
+++ Assignment4/SPML4/src/spml4/Main.java
@@ -22,13 +22,14 @@ public class Main {
         MarkovDecisionProblem mdp = new MarkovDecisionProblem();
         mdp.setInitialState(0, 0);
 
-        Qlearner qLearner = new Qlearner(mdp, 10000, 0.8, 0.2, 0.3);
+        Qlearner qLearner = new Qlearner(mdp, 10000, 1, 0.05, 0.3);
         Action[][] policy = qLearner.run();
         Quple[][] qFunction = qLearner.getQFunction();
 
         for (int y = mdp.getHeight() - 1; y >= 0; y--) {
             for (int x = 0; x < mdp.getWidth(); x++)
-                System.out.printf("%s: \t %.4s \t", qFunction[x][y].getAction(), qFunction[x][y].getValue());
+                System.out.printf("%s ", qFunction[x][y].getAction());
+                //System.out.printf("%s: \t %.4s \t", qFunction[x][y].getAction(), qFunction[x][y].getValue());
             System.out.println("");
         }
 
@@ -39,32 +40,13 @@ public class Main {
         }
         System.out.println("");
         
-        // Second one does not work very well
-        MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
-        mdp2.setField(5, 5, Field.REWARD);
-        Qlearner qLearner2 = new Qlearner(mdp2, 100000, 0.8, 0.2, 0.2);
-        Action[][] policy2 = qLearner2.run();
-        Quple[][] qFunction2 = qLearner2.getQFunction();
-
-        for (int y = mdp2.getHeight() - 1; y >= 0; y--) {
-            for (int x = 0; x < mdp2.getWidth(); x++)
-                System.out.printf("%s: \t %.4s \t", qFunction2[x][y].getAction(), qFunction2[x][y].getValue());
-            System.out.println("");
-        }
-
-        for (int i = 0; i < 100; i++) {
-            mdp2.performAction(policy2[mdp2.getStateXPosition()][mdp2.getStateYPostion()]);
-            if (mdp2.isTerminated())
-                mdp2.restart();
-        }
-        
     }
 
     private static void runValueIterator() {
         MarkovDecisionProblem mdp = new MarkovDecisionProblem();
         mdp.setInitialState(0, 0);
 
-        ValueIterator vi = new ValueIterator(mdp, 0.005, 0.8);
+        ValueIterator vi = new ValueIterator(mdp, 0.2, 1);
         Action[][] policy = vi.run();
 
         for (int y = mdp.getHeight() - 1; y >= 0; y--) {
@@ -79,23 +61,6 @@ public class Main {
                 mdp.restart();
         }
 
-        MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
-        mdp2.setField(5, 5, Field.REWARD);
-        ValueIterator vi2 = new ValueIterator(mdp2, 0.0005, 0.8);
-        Action[][] policy2 = vi2.run();
-
-        System.out.println("");
-        for (int y = mdp2.getHeight() - 1; y >= 0; y--) {
-            for (int x = 0; x < mdp2.getWidth(); x++)
-                System.out.printf("%s ", policy2[x][y]);
-            System.out.println("");
-        }
-
-        for (int i = 0; i < 100; i++) {
-            mdp2.performAction(policy2[mdp2.getStateXPosition()][mdp2.getStateYPostion()]);
-            if (mdp2.isTerminated())
-                mdp2.restart();
-        }
     }
 
 }
